de0bfa063da99c56f8e2446dd68aaf7a80826191
Prathamesh Khade <121573184+Pk-Kolhapurkar@users.noreply.github.com>
2024-08-22 08:47:36 +0530

    Add files via upload

---
+++ modules/commands_answers.py
@@ -0,0 +1,24 @@
+# Commands
+functions = ['what you can do', 'what you do', 'functionalities', 'what you know how to do', 'what else do you know how to do']
+reminders = ['note', 'take a note', 'remind', 'new note', 'new reminder', 'reminder', 'one more note']
+help = ['search', 'i need help', 'help', 'can you help me', 'i have a question']
+time = ['what time is it', 'time', 'time now']
+date = ['what day is it', 'tell me the day']
+analysis_mode = ['emotion mode', 'activate emotion']
+agenda = ['events for today', 'agenda today', 'appointments for today']
+
+commands = [functions, reminders, help, time, date, analysis_mode, agenda]
+#print(comands)
+#print(comands[2])
+
+# Answers
+functionalities = 'Record reminders, do Google searches, speak the time, speak the date, speak events scheduled for today. I also have the analysis mode that when activated, analyzes your emotion based on your voice'
+answers_conclusion = ['Ok!', 'Done!', 'All good!', 'Finished!']
+questions = ['How can I help you?', "Ok, let's go!", 'Right, tell me what you need']
+answers_ok = ['Tell me if you need something else!', 'I am here for you!']
+goodbye = ['I will see you soon!', 'See you later!', 'Bye', 'See you next time']
+
+answers = [functionalities, answers_conclusion, questions, answers_ok, goodbye]
+
+#print(answers)
+#print(answers[2])
